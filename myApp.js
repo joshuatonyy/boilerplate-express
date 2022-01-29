f0784fad9e04e835b4c42097f30ca50217951fc1
joshuatonyy <64730665+joshuatonyy@users.noreply.github.com>
2022-01-29 19:54:53 +0700

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,5 +1,13 @@
 var express = require('express');
 var app = express();
+var absolutePathIndex = __dirname + '/views/index.html';
+app.use('/public', express.static(__dirname + '/public')); //show static files
+app.get('/', (req, res) => { //sendfile from index
+    res.sendFile(absolutePathIndex);
+})
+app.get('/json', (req, res) =>{ //using json
+    res.json({"message": "Hello json"});
+})
 console.log("Hello World");
 
 
